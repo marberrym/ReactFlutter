548b04a1db7d3fb34b16d3f646817bb41b555ace
Matt <marberrym@gmail.com>
2018-09-14 10:12:44 -0400

    Changed all ReactCreate's to JSX syntaxing.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 let h = React.createElement
 let d = ReactDOM.render
 
-let NavBar = () => 
-    h('div', {className: 'navBar'}, 
-        h('div', {className: 'navLink'}, 'About Flutter'),
-        h('div', {className: 'navLink'}, 'Post'),
-    )
+let NavBar = () =>
+    <div className="navBar">
+        <div className="navLink">About Flutter</div>
+        <div className="navLink">Post Flutter</div>
+    </div>
 
 let HeadLogo = () =>
-    h('div', {className: 'logoFlex'}, 
-        h('img', {className: 'headLogo', src: 'images/flutter.png'}),
-        h('div', {className: 'headText'}, 'Flutter')
-    )
+    <div className="logoFlex">
+        <img className="headLogo" src="images/flutter.png"/>
+        <div className="headText">Flutter</div>
+    </div>
 
 class NewPost extends React.Component {
     constructor(props) {
@@ -23,34 +23,38 @@ class NewPost extends React.Component {
         }
     }
     render() {
-        return h('form', {className: 'flexColumn newFlutterBox', onSubmit: (event) => {
-            event.preventDefault(),
-            this.state.submit('Jaydoe', 'images/MillionsKnives.png', this.state.newFlutter)
-            }}, 
-            h('textarea', {placeHolder: '255 max', className: 'postType', value: this.state.newFlutter, onChange: (event) => this.setState({newFlutter: event.target.value})}),
-            h('input', {type: 'submit', className: 'submitBtn', }),
-        )
+        return (<form className="flexColumn newFlutterBox" onSubmit={(event) => {
+            event.preventDefault();
+            return this.state.submit('Jaydoe', 'images/MillionsKnives.png', this.state.newFlutter);
+            }}>
+            <textarea placeholder="255max" className="postType" value={this.state.newFlutter} onChange={
+                (event) => this.setState({newFlutter: event.target.value})
+            }/>
+            <input type="submit" className="submitBtn"/>
+        </form>)
     }
 }  
 
 
 
 let Flutter = (props) =>
-    h('div', {className: 'flutter'}, 
-        h('div', {className: 'logoFlex mZero jStart'}, 
-            h('img', {className: 'iconImg', src: props.icon}),
-            h('div', {className: 'postText'}, props.username, h('div', {}, 'Fluttered')),
-        ),
-        h('p', {}, props.content)
-    )
+    <div className="flutter">
+        <div className="logoFlex mZero jStart">
+            <img className="iconImg" src={props.icon} />
+            <div className="postText">
+                <div>{props.username}</div>
+                <div>Fluttered</div>
+            </div>
+        </div>
+        <p>{props.content}</p>
+    </div>
 
 let PostList = (props) => 
-    h('div', {className: 'postArea'}, [
-        props.postList.map(post => {
-            console.log(post)
-            return h(Flutter, {username: post.username, icon: post.profileImg, content: post.post, key: post.postID})
-        })
-    ])
+    <div className="postArea">
+        {props.postList.map(post => 
+            <Flutter username={post.username} icon={post.profileImg} content={post.post} key={post.postID}/>
+        )}
+    </div>
 
 class FlutterHome extends React.Component {
     constructor(props) {
@@ -90,12 +94,12 @@ class FlutterHome extends React.Component {
             "postID": generateId()}])});
         }
 
-        return h('div', {className: 'flexColumn'}, 
-            h(NavBar),
-            h(HeadLogo),
-            h(NewPost, {submitFlutter}),
-            h(PostList, {postList: this.state.postList.reverse()})
-        )
+        return <div className="flexColumn">
+            <NavBar/>
+            <HeadLogo/>
+            <NewPost submitFlutter={submitFlutter}/>
+            <PostList postList={this.state.postList.reverse()}/>
+        </div>
     }
 }
 
